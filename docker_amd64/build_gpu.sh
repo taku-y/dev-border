ee1cf275d24e174fba7e26fdd3d336eedc7a4d92
taku-y <taku.yoshioka.4096@gmail.com>
2024-12-07 08:51:53 +0900

    Tweaks

--- docker_amd64/build_gpu.sh
+++ docker_amd64/build_gpu.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-docker build --build-arg BASE_IMAGE=nvidia/cuda:11.8.0-cudnn8-devel-ubuntu22.04 -t border .
+docker build \
+    --build-arg BASE_IMAGE=nvidia/cuda:11.8.0-cudnn8-devel-ubuntu22.04 \
+    --build-arg TORCH_URLhttps://download.pytorch.org/whl/test/cu118
+    -t border .
