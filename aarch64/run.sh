6addc8045574ecf9d53878d715f98a7649927211
taku-y <taku.yoshioka.4096@gmail.com>
2025-02-15 01:36:46 +0900

    Tweaks

--- aarch64/run.sh
+++ aarch64/run.sh
@@ -2,6 +2,8 @@ docker run -td  \
     --name border \
     -p 6080:6080 \
     -p 22:22 \
+    -p 8000:8000 \
+    -p 8080:8080 \
     --shm-size=512m \
     --volume="$(pwd)/../../border:/root/border" \
     --volume="$(pwd)/../marimo:/root/marimo" \
