36198cc66359bac0ffc4749e179380e9e6610935
taku-y <taku.yoshioka.4096@gmail.com>
2025-03-01 12:46:15 +0900

    Tweaks

--- docker_amd64/run_gpu.sh
+++ docker_amd64/run_gpu.sh
@@ -1,6 +1,8 @@
 docker run -td --gpus all \
     --name border \
     -p 6080:6080 \
+    -p 2222:22 \
+    -p 8080:8080 \
     --shm-size=512m \
     --volume="$(pwd)/../../border:/root/border" \
     --volume="$(pwd)/../marimo:/root/marimo" \
